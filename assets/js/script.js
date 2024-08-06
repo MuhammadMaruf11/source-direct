1e0b4b9132f426278fdc87aa2e89e5eb945e2a84
MuhammadMaruf11 <muhammad11maruf@gmail.com>
2024-08-06 21:42:15 +0600

    about page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,20 @@
 (function ($) {
     'use strict';
 
-    function initializePlugins() {
-        /*------------------------------------
-            Mobile Menu
-        --------------------------------------*/
-        $('#mobile-menu').meanmenu({
-            meanMenuContainer: '.mobile-menu',
-            meanScreenWidth: "991"
-        });
 
-        $('.toggle-icon ').on('click', function (event) {
-            event.preventDefault();
-            $('.side-bar-menu').toggleClass('active');
-            $(this).toggleClass('open');
-        });
-
-        // $('.toggle-icon > a').on('click', function (event) {
-        //     event.preventDefault();
-        //     $('.side-bar-menu').removeClass('active');
-        // });
-    }
-
-    // Load Header
-    $(function () {
-        $("#header-placeholder").load("includes/header.html", function () {
-            initializePlugins(); // Initialize plugins after header is loaded
-        });
+    /*------------------------------------
+        Mobile Menu
+    --------------------------------------*/
+    $('#mobile-menu').meanmenu({
+        meanMenuContainer: '.mobile-menu',
+        meanScreenWidth: "991"
     });
 
-    // Load Footer
-    $(function () {
-        $("#footer-placeholder").load("includes/footer.html");
+    $('.toggle-icon ').on('click', function (event) {
+        event.preventDefault();
+        $('.side-bar-menu').toggleClass('active');
+        $(this).toggleClass('open');
     });
 
-    // Ensure plugins are initialized after footer content is loaded
-    $(document).on('load', function () {
-        initializePlugins();
-    });
 
 })(jQuery);
